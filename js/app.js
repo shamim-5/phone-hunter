40b61fad0087f9478e9fd5ad104858de01eba1ba
mss38 <email@example.com>
2022-03-01 04:55:41 +0600

    details card added

--- js/app.js
+++ js/app.js
@@ -15,21 +15,54 @@ const searchPhone = (phone) => {
 };
 // display phone in ui with name and brand
 const displaySearchResult = (data) => {
-  //   console.log(data);
+  // console.log(data);
   const searchResult = document.getElementById("search-result");
   data.forEach((product) => {
-    console.log(product);
+    // console.log(product.slug);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-            <div class="card">
-               <img src="${product.image}" class="card-img-top py-3 w-75 h-50 mx-auto" alt="...">
+            <div class="card" >
+               <img src="${product.image}" class="card-img-top shadow py-3 w-50 h-50 mx-auto" alt="...">
                <div class="card-body">
                   <h5 class="card-title">Name: ${product.phone_name}</h5>
                   <p class="card-text">Brand: ${product.brand}</p>
+               </div>
+               <div class="d-grid justify-content-end p-2">
+                  <button onclick="loadProductDetails('${product.slug}')" class="btn btn-info fw-bold" type="button">More Details</button>
                </div>
             </div>
             `;
     searchResult.appendChild(div);
   });
 };
+
+const loadProductDetails = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayProductDetails(data.data));
+};
+
+const displayProductDetails = (product) => {
+  console.log(product);
+  const productDetails = document.getElementById("product-details");
+  const div = document.createElement("div");
+  div.classList.add("col");
+  div.innerHTML = `
+            <div class="card shadow" >
+               <img src="${product.image}" class="card-img-top p-3" alt="...">
+               <div class="card-body">
+                  <p class="card-text">Id: ${product.slug}</p>
+                  <h5 class="card-title">Name: ${product.name}</h5>
+                  <p class="card-text">Brand: ${product.brand}</p>
+                  <p class="card-text">Storage: ${product.mainFeatures.storage}</p>
+                  <p class="card-text">Display: ${product.mainFeatures.displaySize}</p>
+                  <p class="card-text">Chipset: ${product.mainFeatures.chipSet}</p>
+               </div>
+            </div>
+            `;
+  productDetails.appendChild(div);
+};
+
